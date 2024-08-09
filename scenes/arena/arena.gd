5616d211e612bda38572ebc38ccb815ab95d61ec
Adam Gulacsi <adam.gulacsi@gmail.com>
2024-08-09 09:35:17 +0200

    sfx and music implemented

--- scenes/arena/arena.gd
+++ scenes/arena/arena.gd
@@ -5,6 +5,8 @@ const CELL_SIZE := Vector2(32, 32)
 const HALF_CELL_SIZE := Vector2(16, 16)
 const QUARTER_CELL_SIZE := Vector2(8, 8)
 
+@export var arena_music_stream: AudioStream
+
 @onready var sell_portal: SellPortal = $SellPortal
 @onready var unit_mover: UnitMover = $UnitMover
 @onready var unit_adder: UnitAdder = $UnitAdder
@@ -17,6 +19,8 @@ func _ready() -> void:
 	unit_adder.unit_added.connect(unit_mover.setup_unit)
 	unit_adder.unit_added.connect(unit_combiner.queue_unit_combination_update.unbind(1))
 	shop.unit_bought.connect(unit_adder.add_unit)
+	
+	MusicPlayer.play(arena_music_stream)
 
 
 # FIXME rand_weighted will be added in Godot 4.3 
