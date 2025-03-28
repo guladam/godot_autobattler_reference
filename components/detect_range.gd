31b5bbc98c995f0ba5e9a2638569041bb3dac201
Adam Gulacsi <adam.gulacsi@gmail.com>
2025-03-28 10:42:04 +0100

    robin cast ability finished

--- components/detect_range.gd
+++ components/detect_range.gd
@@ -2,14 +2,13 @@ class_name DetectRange
 extends Area2D
 
 @export var col_shape: CollisionShape2D
-@export var base_range_size: Vector2
+@export var base_range_size: float
 @export var stats: UnitStats:
 	set(value):
 		stats = value
 		collision_layer = 4 * (stats.team + 1)
 		collision_mask = 2 - stats.team
 		
-		# TODO is a circle actually better?
-		var shape := RectangleShape2D.new()
-		shape.size = base_range_size * stats.attack_range
+		var shape := CircleShape2D.new()
+		shape.radius = base_range_size * stats.attack_range
 		col_shape.shape = shape
--- scenes/_abilities/robin_hail_of_arrows.gd
+++ scenes/_abilities/robin_hail_of_arrows.gd
@@ -1,12 +1,30 @@
 extends UnitAbility
 
+@export var extra_shots_per_tier: Array[int]
 
 func use() -> void:
 	var all_enemies := get_tree().get_nodes_in_group(UnitStats.TARGET[caster.stats.team])
-	all_enemies.shuffle()
-	for enemy: BattleUnit in all_enemies.slice(0, 3):
-		var projectile := caster.ranged_attack.attack(enemy.global_position) as Projectile
-		projectile.target = enemy.global_position
-		projectile.hitbox.damage = caster.stats.get_attack_damage()
+	var tween := caster.create_tween()
+	
+	for _i in extra_shots_per_tier[caster.stats.tier-1]:
+		tween.tween_interval(0.15)
+		tween.tween_callback(_spawn_extra_projectile.bind(all_enemies))
 
-	ability_cast_finished.emit.call_deferred()
+	tween.finished.connect(ability_cast_finished.emit)
+
+
+func _spawn_extra_projectile(enemies: Array[Node]) -> void:
+	if enemies.is_empty():
+		return
+	
+	var valid_enemies = enemies.filter(
+		func(obj): return is_instance_valid(obj)
+	)
+	
+	if valid_enemies.is_empty():
+		return
+	
+	var enemy := valid_enemies.pick_random() as BattleUnit
+	var projectile := caster.ranged_attack.attack(enemy.global_position) as Projectile
+	projectile.target = enemy.global_position
+	projectile.hitbox.damage = caster.stats.get_attack_damage()
